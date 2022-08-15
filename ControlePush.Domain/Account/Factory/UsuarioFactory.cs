c7aeb4a74ec8c03fb00cb8d047b4fa4c9fcbf0d2
Igor Lessa Robaina De Souza <igor.lessa.souza@accenture.com>
2022-08-15 18:03:56 -0300

    factory

---
+++ ControlePush.Domain/Account/Factory/UsuarioFactory.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ControlePush.Domain.Account.Factory
+{
+    internal class UsuarioFactory
+    {
+    }
+}
